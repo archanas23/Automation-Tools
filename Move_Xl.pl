63751c9796ed41412031f93f8b4b5006aeabb6ca
Archana Soundararajan <archanas@umd.edu>
2015-10-27 12:07:42 -0400

    Create Move_Xl.pl
    Added Move_XL.pl file

---
+++ Move_Xl.pl
@@ -0,0 +1,36 @@
+#!/user/bin/perl
+
+use Cwd;
+use File::Copy;
+
+#get the path of the folder of the source file
+$path=getcwd();
+$dir = "$path/FVT"; 
+$dir_Xl = "$path/FVT Scenarios";
+
+#open the directory of the source file
+opendir(DIR, $dir_Xl) or die "can't opendir $dir: $! \n";
+
+#read the files in the current directory
+@file_of_dir=readdir(DIR);
+
+#close the directory
+closedir DIR;
+#print "@file_of_dir\n";
+
+#search for the file that is needed to move
+foreach $file (@file_of_dir)
+{
+if($file=~ m/^FVT_/){
+$folder=$file;
+$folder=~ s/_TP.xls$//;
+$folder=~ s/_TP.xlsx$//;
+$source = "$dir_Xl/$file";
+$target = "$dir/$folder";
+
+#copy it to the destination folder
+copy $source => $target or warn "Copy of $file failed: $!";
+}
+}
+
+
